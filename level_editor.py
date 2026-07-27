63a4d4ddc635a7ebbf83be28bd56a6b6126cb416
Sh1onSato <k024g0077@g.neec.ac.jp>
2026-07-27 12:09:10 +0900

    Update level_editor.py
    TL1_09

--- level_editor.py
+++ level_editor.py
@@ -1,6 +1,10 @@
 import bpy
 import math
 import bpy_extras
+import gpu
+import gpu_extras.batch
+import copy
+
 
 # ブレンダーに登録するアドオン情報
 bl_info = {
@@ -172,6 +176,76 @@ class OBJECT_PT_file_name(bpy.types.Panel):
         else:
             # プロパティがなければ、追加するためのボタンを表示
             self.layout.operator(MYADDON_OT_add_filename.bl_idname)
+
+# コライダー描画クラス
+class DrawCollider:
+    # 描画ハンドル
+    handle = None
+
+    # 3Dビューに登録する描画関数
+    def draw_collider():
+        # 頂点データ・インデックスデータ
+        vertices = {"pos": []}
+        indices = []
+
+        # 各頂点の、オブジェクト中心からのオフセット
+        offsets = [
+            [-0.5, -0.5, -0.5], # 左下前
+            [+0.5, -0.5, -0.5], # 右下前
+            [-0.5, +0.5, -0.5], # 左上前
+            [+0.5, +0.5, -0.5], # 右上前
+            [-0.5, -0.5, +0.5], # 左下奥
+            [+0.5, -0.5, +0.5], # 右下奥
+            [-0.5, +0.5, +0.5], # 左上奥
+            [+0.5, +0.5, +0.5], # 右上奥
+        ]
+
+        # 立方体のX, Y, Z方向サイズ
+        size = [2, 2, 2]
+
+        # 現在シーンのオブジェクトリストを走査
+        for object in bpy.context.scene.objects:
+            # 追加前の頂点数（インデックスのベースオフセット）
+            start = len(vertices["pos"])
+
+            # Boxの8頂点分回す
+            for offset in offsets:
+                # オブジェクトの中心座標をコピー
+                pos = copy.copy(object.location)
+                # 中心点を基準に各頂点ごとにずらす
+                pos[0] += offset[0] * size[0]
+                pos[1] += offset[1] * size[1]
+                pos[2] += offset[2] * size[2]
+                # 頂点データリストに座標を追加
+                vertices['pos'].append(pos)
+
+            # 前面を構成する辺の頂点インデックス
+            indices.append([start + 0, start + 1])
+            indices.append([start + 2, start + 3])
+            indices.append([start + 0, start + 2])
+            indices.append([start + 1, start + 3])
+
+            # 奥面を構成する辺の頂点インデックス
+            indices.append([start + 4, start + 5])
+            indices.append([start + 6, start + 7])
+            indices.append([start + 4, start + 6])
+            indices.append([start + 5, start + 7])
+
+            # 手前と奥を繋ぐ辺の頂点インデックス
+            indices.append([start + 0, start + 4])
+            indices.append([start + 1, start + 5])
+            indices.append([start + 2, start + 6])
+            indices.append([start + 3, start + 7])
+
+        shader = gpu.shader.from_builtin("UNIFORM_COLOR")
+        batch = gpu_extras.batch.batch_for_shader(shader, "LINES", vertices, indices=indices)
+
+        color = [0.5, 1.0, 1.0, 1.0]
+        shader.bind()
+        shader.uniform_float("color", color)
+        
+        batch.draw(shader)
+
 classes = (
     TOPBAR_MT_my_menu,
     MYADDON_OT_stretch_vertex,
@@ -185,10 +259,16 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
     bpy.types.TOPBAR_MT_editor_menus.append(submenu)
+    DrawCollider.handle = bpy.types.SpaceView3D.draw_handler_add(
+        DrawCollider.draw_collider, (), "WINDOW", "POST_VIEW"
+    )
     print("レベルエディタが有効化されました。")
 
 def unregister():
     bpy.types.TOPBAR_MT_editor_menus.remove(submenu)
+    if DrawCollider.handle is not None:
+        bpy.types.SpaceView3D.draw_handler_remove(DrawCollider.handle, "WINDOW")
+        DrawCollider.handle = None
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
